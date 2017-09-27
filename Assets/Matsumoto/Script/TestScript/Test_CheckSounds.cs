4e2bce8c874888377b60d6b3fcd25913ed4e6758
Matsumoto <nkc.ryo.matsumoto@gmail.com>
2017-09-27 12:08:19 +0900

    AudioManager実装完了

---
+++ Assets/Matsumoto/Script/TestScript/Test_CheckSounds.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test_CheckSounds : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		AudioManager.Play(BGMType.Title, 1.0f, true);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
