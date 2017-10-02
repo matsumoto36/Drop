1ec9e8c0162213be922f810f9e8cb77441700f29
daimurata <nkc.dai.murata@gmail.com>
2017-10-02 10:15:33 +0900

    ダミーの作成完了

---
+++ Assets/Murata/Script/HoleDummy.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoleDummy : MonoBehaviour {
+
+    //穴のプレファブ
+    public GameObject EnemyHole;
+
+
+
+    void Awake()
+    {
+        //プレファブを同ポジションに生成
+        GameObject go = (GameObject)Instantiate(
+            EnemyHole,
+            Vector3.zero,
+            Quaternion.identity
+            );
+        go.transform.localScale = transform.localScale;
+        //一緒に削除されるように生成した敵オブジェクトを子に設定
+        go.transform.SetParent(transform, false);
+
+    }
+
+    //ステージエディット中のためにシーンにキズモを表示
+    void OnDrawGizmos()
+    {
+       
+
+        //球を表示
+        //球の色
+        Gizmos.color = new Color(1, 0, 0, 0.5f);
+
+        Gizmos.DrawSphere(this.transform.position ,transform.localScale.x);
+
+        //穴のプレファブのアイコンを表示
+        if (EnemyHole != null)
+            Gizmos.DrawIcon(transform.position , EnemyHole.name, true);
+       
+    }
+}
