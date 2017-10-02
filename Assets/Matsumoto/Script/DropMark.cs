83fee18e04bf7ea9ec19cac332c2d2ade1c2c1ab
Matsumoto <nkc.ryo.matsumoto@gmail.com>
2017-10-02 12:33:53 +0900

    雫が通った跡が出ますよ

---
+++ Assets/Matsumoto/Script/DropMark.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DropMark : MonoBehaviour {
+
+	public float multiPly;
+	public Vector2 dir;
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine(FadeDropMark());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	IEnumerator FadeDropMark() {
+
+		var markRenderer = GetComponent<SpriteRenderer>();
+		var startSize = transform.localScale;
+
+		var t = 0.0f;
+		while(t < 1.0f) {
+			t += Time.deltaTime * multiPly / startSize.x;
+
+			var col = markRenderer.color;
+			col.a = 1 - t;
+			markRenderer.color = col;
+
+			transform.localScale = Vector3.Lerp(new Vector3(), startSize, 1 - t);
+
+			yield return null;
+		}
+
+		Destroy(gameObject);
+	}
+}
