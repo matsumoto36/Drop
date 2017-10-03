f9db5d758c45fe3f88d5a041ae57f03702633474
eru <eru.birdsea@gmail.com>
2017-10-03 14:27:45 +0900

    ポーズ機能完成

---
+++ Assets/chiikama/script/BGMvolume.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BGMvolume : MonoBehaviour
+{
+
+	[SerializeField]
+	UnityEngine.Audio.AudioMixer mixer;
+
+	void Start()
+	{
+		float bgmVolume;
+		mixer.GetFloat("BGM", out bgmVolume);
+		GetComponent<Slider>().value = bgmVolume;
+
+	}
+
+	public float BGMVolume
+	{
+		set
+		{
+			Debug.Log(value);
+			mixer.SetFloat("BGMVolume", value);
+		}
+
+	}
+
+	void Update()
+	{
+	}
+}
+
+
+		
