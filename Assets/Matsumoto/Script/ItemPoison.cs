efe236f9e9701ea85c50b99d74bec5bf39f433f6
Matsumoto <nkc.ryo.matsumoto@gmail.com>
2017-10-04 03:00:39 +0900

    アイテム実装

---
+++ Assets/Matsumoto/Script/ItemPoison.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemPoison : MonoBehaviour {
+	Player player;
+	void Start()
+	{
+		player = FindObjectOfType<Player>();
+	}
+
+	void OnTriggerEnter2D(Collider2D col)
+	{
+		if (col.gameObject.tag == "Player")
+		{
+					Debug.Log("poison");
+					player.SetStatus(PlayerStatus.Poison, 10.0f);
+			        Destroy(gameObject);
+		}
+
+			}
+
+		}
+	
+	// Use this for initialization
+
+
+	
+	// Update is called once per frame
+
+
